378c0ebe719fbae7329bf7e12fef9d03da02693c
xayrullacholiyev883-sudo <xayrullacholiyev883@gmail.com>
2026-09-20 15:34:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -68,6 +68,6 @@ async def sections_handler(message: types.Message):
 if __name__ == "__main__":
     from aiogram import executor
     print("Bot tayyor!")
-    pollingling(dp)
+executor.start_polling(dp)
 
 
