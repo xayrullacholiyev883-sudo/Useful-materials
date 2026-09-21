dc4300e796177ddb694a58ce341a0b574c4b5310
xayrullacholiyev883-sudo <xayrullacholiyev883@gmail.com>
2026-09-21 11:30:22 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 # Siz taqdim etgan ma'lumotlar
-API_TOKEN = "8938280108:AAHRftEIUXStQdjY4q90-zFMIo5tH3XAzys"  # O'z tokeningizni yozing
+API_TOKEN = "8938280108:AAG3j4pmmhAnFtp1r91I4bp8Gx19RU5xqio"  # O'z tokeningizni yozing
 ADMIN_ID = 8243336938
 ADMIN_USERNAME = "narzullayevich_2010"
 
